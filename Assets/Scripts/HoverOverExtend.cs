70c3f141165b592c6c61b0f3fc4c7718a79341ea
Jandromon <krikxela@hotmail.com>
2023-01-02 02:51:18 +0100

    Jandro_02_01

--- Assets/Scripts/HoverOverExtend.cs
+++ Assets/Scripts/HoverOverExtend.cs
@@ -10,12 +10,12 @@ public class HoverOverExtend : MonoBehaviour//, IPointerEnterHandler, IPointerEx
     public void Enter()
     {
         transform.GetComponent<RectTransform>().sizeDelta = new Vector2(130, 11);
-        transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(9.17f, 2.47f, 0);
+        transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.0841f, 2.98f, 0);
     }
 
     public void Exit()
     {
-        transform.GetComponent<RectTransform>().sizeDelta = new Vector2(60, 11);
-        transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(1.74f, 2.47f, 0);
+        transform.GetComponent<RectTransform>().sizeDelta = new Vector2(62.5f, 11);
+        transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(-0.0841f, 2.98f, 0);
     }
 }
