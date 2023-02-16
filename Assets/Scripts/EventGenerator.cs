90332669dc52d9170fc323d607fcab36f909aa98
Jandromon <krikxela@hotmail.com>
2023-02-16 18:21:12 +0000

    Jandro_16_02

--- Assets/Scripts/EventGenerator.cs
+++ Assets/Scripts/EventGenerator.cs
@@ -8,16 +8,18 @@ public class EventGenerator : MonoBehaviour
     public void GenerateEvent()
     {
         float number = Random.Range(0f, 100f);
-        if (number < 33f)
+        if (number < 30f)
             this.transform.GetChild(2).GetComponent<Text>().text = "Normal Enemy";
-        if (number >= 33f && number < 43f)
+        if (number >= 30f && number < 40f)
             this.transform.GetChild(2).GetComponent<Text>().text = "Strong Enemy";
-        if (number >= 43f && number < 62f)
+        if (number >= 40f && number < 58f)
             this.transform.GetChild(2).GetComponent<Text>().text = "Market";
-        if (number >= 62f && number < 79f)
+        if (number >= 58f && number < 75f)
             this.transform.GetChild(2).GetComponent<Text>().text = "Hangar";
-        if (number >= 79f && number < 98f)
+        if (number >= 75f && number < 92f)
             this.transform.GetChild(2).GetComponent<Text>().text = "Rare Event";
+        if (number >= 92f && number < 98f)
+            this.transform.GetChild(2).GetComponent<Text>().text = "Event Enemy";
         if (number >= 98f)
             this.transform.GetChild(2).GetComponent<Text>().text = "Ultra Rare Event";
     }
